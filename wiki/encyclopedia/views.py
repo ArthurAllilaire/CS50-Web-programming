6ee277db546fd5893d92a7023e44d95265b7eca4
Arthur Allilaire <arthur.allilaire@gmail.com>
2021-06-05 10:45:54 +0100

    Changed Search form to django form in backend

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,12 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponseNotFound
+from django import forms
 
 from . import util
 
+class SearchForm(forms.Form):
+    q = forms.CharField(
+        label="",
+        # Adds class search to the form - from: https://treyhunner.com/2014/09/adding-css-classes-to-django-form-fields/, other solution could be used to have classnames only in front end, not backend.
+        widget=forms.TextInput(attrs={'class': "search", "placeholder": "Search Encyclopedia"}),
+        
+    )
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
+        "entries": util.list_entries(),
+        "form": SearchForm()
     })
 
 def article(request, title):
@@ -17,5 +26,8 @@ def article(request, title):
         "title": title,
         "article": article
     })
+
+def search(request):
+    return render(request, "encyclopedia/search.html")
 
 
