71512a810d5cefc3aa78c39d368b4b2b40d12111
Arthur Allilaire <arthur.allilaire@gmail.com>
2021-06-08 08:28:36 +0100

    Finished models for now

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -1,6 +1,26 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.db.models.deletion import CASCADE, SET_NULL
 
 
 class User(AbstractUser):
     pass
+
+class Listing(models.Model):
+    #I want to create new listing - one ot many relationship between comments and listing - done in comment model
+    #one to many relationship for user to listing
+    owner = models.ForeignKey(User, on_delete=CASCADE),
+    title = models.CharField(max_length=100),
+    description = models.CharField(max_length=1000),
+    price = models.IntegerField(),
+    image = models.ImageField(verbose_name="Product image", blank=True),
+    category = models.CharField(max_length=100, blank=True)
+
+class Comment(models.Model):
+    listing = models.ForeignKey(Listing, on_delete=CASCADE),
+    user = models.ForeignKey(User, null=True, on_delete=SET_NULL)
+
+class Bid(models.Model):
+    listing = models.ForeignKey(Listing, on_delete=CASCADE),
+    price = models.FloatField()
+
